720620a7aa9aa972dee34fd43eedbfaca4f1dfd5
nikigor <n.gorbadey@gmail.com>
2019-01-28 01:17:01 +0300

    dynamixel.c: Add error catcher implementation
    1) Implement DMA with USART receive
    2) Add timer for error checker
    3) Implement error checker
    
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- lib/dynamixel.c
+++ lib/dynamixel.c
@@ -2,11 +2,11 @@
 #include "fsm.h"
 #include "gpio_map.h"
 #include "peripheral.h"
+#include "err_manager.h"
 
 #include "stm32f0xx_ll_rcc.h"
 #include "stm32f0xx_ll_bus.h"
-#include "stm32f0xx_ll_usart.h"
-#include "stm32f0xx_ll_dma.h"
+
 /*
  * Private control structure
  */
@@ -15,10 +15,14 @@ static dyn_ctrl_t dyn_ctrl;
 /*
  * Private functions
  */
+/*
+ * Send command
+ */
 static void dyn_send_cmd(uint8_t *buff, int len)
 {
         int i = 0;
 
+        LL_USART_DisableDirectionRx(DYNAMIXEL_USART);
         LL_USART_EnableDirectionTx(DYNAMIXEL_USART);
         LL_USART_ClearFlag_TC(DYNAMIXEL_USART);
         while (len--) {
@@ -28,7 +32,19 @@ static void dyn_send_cmd(uint8_t *buff, int len)
         while (!LL_USART_IsActiveFlag_TC(DYNAMIXEL_USART));
         LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
         LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
-        dyn_ctrl.rx_complete = 0;
+        dyn_clr_flag(dyn_ctrl, RX_COMPLETE);
+        return;
+}
+
+/*
+ * Send request for status packet from dynamixel by id
+ */
+static void dyn_ping(uint8_t id)
+{
+        uint8_t crc = id + 0x02 + 0x01;
+        uint8_t tx[] = {0xff, 0xff, id, 0x02, 0x01, ~crc};
+
+        dyn_send_cmd(tx, 6);
         return;
 }
 
@@ -42,11 +58,38 @@ static void dyn_send_cmd(uint8_t *buff, int len)
  */
 void fsm_dynamixel_error_catch(void *args)
 {
-        (void) args;
-
         /*
          * Check for errors here
          */
+        (void) args;
+        static uint8_t cur_id = 1;
+        static dyn_status_pack_t *dyn_stat;
+
+        if (is_dyn_flag_set(dyn_ctrl, RX_COMPLETE)) {
+                /*
+                 * Clear RX_COMPLETE flag
+                 */
+                dyn_clr_flag(dyn_ctrl, RX_COMPLETE);
+                dyn_clr_flag(dyn_ctrl, CHECK_DYNAMIXEL);
+                /*
+                 * Process status packet and save status
+                 */
+                dyn_stat = (dyn_status_pack_t *) dyn_ctrl.channel_rx;
+                dyn_ctrl.status[dyn_stat->id] = dyn_stat->err;
+                if (dyn_stat->err) {
+                        err_man_update_dyn_status(dyn_stat->id, dyn_stat->err);
+                        fsm_set_state(FSM_ERR_MAN_SHOW_ERR);
+                }
+                /*
+                 * Request status packet from next dynamixel if tx is not busy
+                 */
+                if (!is_dyn_flag_set(dyn_ctrl, TX_BUSY) &&
+                    is_dyn_flag_set(dyn_ctrl, CHECK_DYNAMIXEL)) {
+                        dyn_ping(cur_id++);
+                        if (cur_id > NUMBER_OF_DYNAMIXELS)
+                                cur_id = 1;
+                }
+        }
         return;
 }
 
@@ -59,12 +102,12 @@ void fsm_dynamixel_error_catch(void *args)
 void fsm_dynamixel_init(void *args)
 {
         (void) args;
+        int i = 0;
 
         fsm_add_shadow_state(FSM_DYNAMIXEL_ERROR_CATCH);
         /*
          * Initialization code
          */
-
         /*
          * Setting USART pin
          */
@@ -90,9 +133,10 @@ void fsm_dynamixel_init(void *args)
         LL_USART_SetBaudRate(DYNAMIXEL_USART, SystemCoreClock,
                              DYNAMIXEL_USART_OVERSAMPL,
                              DYNAMIXEL_USART_BAUDRATE);
-        LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
-        LL_USART_EnableHalfDuplex(DYNAMIXEL_USART);
         LL_USART_EnableDMAReq_RX(DYNAMIXEL_USART);
+        LL_USART_EnableHalfDuplex(DYNAMIXEL_USART);
+        LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
+        LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
         /*
          * DMA RX configuration
          */
@@ -102,49 +146,57 @@ void fsm_dynamixel_init(void *args)
                                (uint32_t) dyn_ctrl.channel_rx,
                                DYNAMIXEL_DMA_RX_DIRECTION);
         LL_DMA_SetDataLength(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL,
-                             DYNAMIXEL_DMA_RX_BUFFER_SIZE);
+                            DYNAMIXEL_DMA_RX_BUFFER_SIZE);
         LL_DMA_SetMemoryIncMode(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL,
-                                DYNAMIXEL_DMA_RX_MEM_INC_MODE);
+                               DYNAMIXEL_DMA_RX_MEM_INC_MODE);
+        LL_DMA_SetMode(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL,
+                       LL_DMA_MODE_CIRCULAR);
         LL_DMA_EnableIT_TC(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL);
         /*
-         * DMA TX configuration
+         * Timer setting for dynamixel error catcher
          */
-        // LL_DMA_ConfigAddresses(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL,
-        //                        (uint32_t) dyn_ctrl.channel_tx,
-        //                        DYNAMIXEL_DMA_RX_DEST_ADDR,
-        //                        DYNAMIXEL_DMA_TX_DIRECTION);
-        // LL_DMA_SetDataLength(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL,
-        //                      DYNAMIXEL_DMA_TX_BUFFER_SIZE);
-        // LL_DMA_SetPeriphSize(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL,
-        //                      LL_DMA_PDATAALIGN_BYTE);
-        // LL_DMA_SetMemoryIncMode(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL,
-        //                         DYNAMIXEL_DMA_TX_MEM_INC_MODE);
-        // LL_DMA_SetPeriphIncMode(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL,
-        //                         DYNAMIXEL_DMA_RX_PERIPH_INC_MODE);
-        // LL_DMA_EnableIT_TC(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL);
+        LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
+        LL_TIM_SetCounterMode(DYNAMIXEL_TIMER, DYNAMIXEL_TIMER_MODE);
+        LL_TIM_SetAutoReload(DYNAMIXEL_TIMER, DYNAMIXEL_TIMER_RELOAD);
+        LL_TIM_SetPrescaler(DYNAMIXEL_TIMER, DYNAMIXEL_TIMER_PSC);
+        LL_TIM_EnableIT_UPDATE(DYNAMIXEL_TIMER);
         /*
-         * USART and DMA turn on
+         * Setup DMA interrupt
          */
-        LL_USART_Enable(DYNAMIXEL_USART);
-        LL_DMA_EnableChannel(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL);
-        //LL_DMA_EnableChannel(DYNAMIXEL_DMA_TX, DYNAMIXEL_DMA_TX_CHANNEL);
-        /* 
-         * Enable global USART and DMA stream interrupts
-         */
-        NVIC_SetPriority(DYNAMIXEL_USART_IRQN, DYNAMIXEL_USART_IRQN_PRIORITY);
-        NVIC_EnableIRQ(DYNAMIXEL_USART_IRQN);
         NVIC_SetPriority(DYNAMIXEL_DMA_CHANNEL_IRQN,
                          DYNAMIXEL_DMA_CHANNEL_IRQN_PRIORITY);
         NVIC_EnableIRQ(DYNAMIXEL_DMA_CHANNEL_IRQN);
-
+        /*
+         * Setup Timer interrupt
+         */
+        //NVIC_SetPriority(DYNAMIXEL_TIMER_IRQN, DYNAMIXEL_TIMER_IRQN_PRIORITY);
+        //NVIC_EnableIRQ(DYNAMIXEL_TIMER_IRQN);
+        /*
+         * Turn on USART, DMA and Timer
+         */
+        LL_DMA_EnableChannel(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL);
+        LL_USART_Enable(DYNAMIXEL_USART);
+        LL_TIM_EnableCounter(DYNAMIXEL_TIMER);
+        /*
+         * Clear rx complete flag
+         */
+        dyn_set_flag(dyn_ctrl, RX_COMPLETE);
+        dyn_clr_flag(dyn_ctrl, CHECK_DYNAMIXEL);
+        dyn_clr_flag(dyn_ctrl, TX_BUSY);
+        /*
+         * Clear status flags
+         */
+        for (i = 0; i < NUMBER_OF_DYNAMIXELS; i++)
+                dyn_ctrl.status[i] = 0;
+        /*
+         * Configure debug led
+         */
         LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
         LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
         LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
 
-        dyn_ctrl.rx_complete = 0;
-
         /*
-         * Switch to init terminal
+         * Switch to init collision avoidance
          */
         fsm_set_state(FSM_COLL_AVOID_INIT);
         return;
@@ -155,24 +207,51 @@ void fsm_dynamixel_init(void *args)
  */
 void fsm_dyn_set_angle(void *args)
 {
+        /*
+         * Make new packet
+         */
         dyn_set_angle_t *cmd_args = (dyn_set_angle_t *) args;
-        if (cmd_args->angle > 0x3fff)
-                return;
         uint8_t highByte = (uint8_t)((cmd_args->angle >> 8) & 0xff);
         uint8_t lowByte = (uint8_t)(cmd_args->angle & 0xff);
         uint8_t crc = cmd_args->id + 0x05 + 0x03 + 0x1e + lowByte + highByte;
         uint8_t tx[] = {0xff, 0xff, cmd_args->id, 0x05, 0x03, 0x1e, lowByte,
                                highByte, ~crc};
+
+        if (cmd_args->angle > 0x3fff)
+                goto set_angle_error;
+        /*
+         * Set tx busy flag
+         */
+        dyn_set_flag(dyn_ctrl, TX_BUSY);
+        /*
+         * If receive is not complete, wait until error catcher process status
+         * packet, don't switch to another state
+         */
+        if (is_dyn_flag_set(dyn_ctrl, RX_COMPLETE))
+                return;
         dyn_send_cmd(tx, 9);
+        fsm_set_state(FSM_TERM_MAIN);
+set_angle_error:
+        fsm_set_state(FSM_TERM_MAIN);
         return;
 }
 
+/*
+ * Hardware interrupt handlers
+ */
 void DMA1_Channel2_3_IRQHandler(void)
 {
         if (LL_DMA_IsActiveFlag_TC3(DYNAMIXEL_DMA_RX)) {
                 LL_DMA_ClearFlag_TC3(DYNAMIXEL_DMA_RX);
-                LL_DMA_ClearFlag_HT3(DYNAMIXEL_DMA_RX);
-                LL_DMA_EnableChannel(DYNAMIXEL_DMA_RX, DYNAMIXEL_DMA_RX_CHANNEL);
-                dyn_ctrl.rx_complete = 1;
+                dyn_set_flag(dyn_ctrl, RX_COMPLETE);
+                dyn_clr_flag(dyn_ctrl, TX_BUSY);
+        }
+}
+
+void TIM3_IRQHandler(void)
+{
+        if (LL_TIM_IsActiveFlag_UPDATE(DYNAMIXEL_TIMER)) {
+                LL_TIM_ClearFlag_UPDATE(DYNAMIXEL_TIMER);
+                dyn_set_flag(dyn_ctrl, CHECK_DYNAMIXEL);
         }
 }
--- lib/dynamixel.h
+++ lib/dynamixel.h
@@ -2,6 +2,7 @@
 #define __DYNAMIXEL_H__
 
 #include <stdint.h>
+#include "dev_map.h"
 
 #define MAX_PACK_REQ_LENGTH     32
 #define MAX_PACK_RESP_LENGTH    32
@@ -15,14 +16,45 @@ typedef struct {
 }__attribute__((packed)) dyn_set_angle_t;
 
 /*
- * Dynamixel control struxture
+ * Dynamixel status packet structure
  */
 typedef struct {
-        uint8_t channel_rx[MAX_PACK_RESP_LENGTH];
-        uint8_t channel_tx[MAX_PACK_REQ_LENGTH];
-        uint8_t rx_complete;
-} dyn_ctrl_t;
+        uint8_t header1;
+        uint8_t header2;
+        uint8_t id;
+        uint8_t len;
+        uint8_t err;
+        uint8_t crc;
+}__attribute__((packed)) dyn_status_pack_t;
 
-void fsm_dynamixel_init(void *args);
+/*
+ * Flags for dynamixel
+ */
+#define RX_COMPLETE_POS         (0U)
+#define TX_BUSY_POS             (1U)
+#define CHECK_DYNAMIXEL_POS     (2U)
+
+#define RX_COMPLETE             (0x01 << RX_COMPLETE_POS)
+#define TX_BUSY                 (0x01 << TX_BUSY_POS)
+#define CHECK_DYNAMIXEL         (0x01 << CHECK_DYNAMIXEL_POS)
+
+#define is_dyn_flag_set(dyn_ctrl, bit) \
+        (dyn_ctrl.flags & bit)
+
+#define dyn_set_flag(dyn_ctrl, bit)  \
+        dyn_ctrl.flags |= bit
+
+#define dyn_clr_flag(dyn_ctrl, bit) \
+        dyn_ctrl.flags &= (~bit)
+
+/*
+ * Dynamixel control structure
+ */
+typedef struct {
+        uint8_t channel_rx[MAX_PACK_REQ_LENGTH];
+        uint8_t channel_tx[MAX_PACK_REQ_LENGTH];
+        uint8_t status[NUMBER_OF_DYNAMIXELS];
+        uint8_t flags;
+} dyn_ctrl_t;
 
 #endif
