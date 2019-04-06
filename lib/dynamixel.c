54a0c1dc303a78c6956a7f39c6e892284c5713f7
Edgar Kaziakhmedov <edos.edgar@gmail.com>
2019-04-06 20:31:01 +0300

    Switch to 4space tabs
    Signed-off-by: Edgar Kaziakhmedov <edos.edgar@gmail.com>

--- lib/dynamixel.c
+++ lib/dynamixel.c
@@ -15,53 +15,53 @@
  */
 static void dyn_send_cmd(uint8_t *buff, int len)
 {
-        int i = 0;
+    int i = 0;
 
-        LL_USART_DisableDirectionRx(DYNAMIXEL_USART);
-        LL_USART_EnableDirectionTx(DYNAMIXEL_USART);
-        LL_USART_ClearFlag_TC(DYNAMIXEL_USART);
-        while (len--) {
-                while (!LL_USART_IsActiveFlag_TXE(DYNAMIXEL_USART));
-                LL_USART_TransmitData8(DYNAMIXEL_USART, buff[i++]);
-        }
-        while (!LL_USART_IsActiveFlag_TC(DYNAMIXEL_USART));
-        LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
-        LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
-        return;
+    LL_USART_DisableDirectionRx(DYNAMIXEL_USART);
+    LL_USART_EnableDirectionTx(DYNAMIXEL_USART);
+    LL_USART_ClearFlag_TC(DYNAMIXEL_USART);
+    while (len--) {
+            while (!LL_USART_IsActiveFlag_TXE(DYNAMIXEL_USART));
+            LL_USART_TransmitData8(DYNAMIXEL_USART, buff[i++]);
+    }
+    while (!LL_USART_IsActiveFlag_TC(DYNAMIXEL_USART));
+    LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
+    LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
+    return;
 }
 
 static void dyn_delay(uint32_t i)
 {
-        while(i--);
-        return;
+    while(i--);
+    return;
 }
 
 static void dyn_set_speed(uint8_t id, uint16_t speed)
 {
-        /*
-         * Make new packet
-         */
-        static const uint8_t DYN_SET_ANGLE_CMD_LEN = 9;
-        uint8_t highByte = (uint8_t)((speed >> 8) & 0xff);
-        uint8_t lowByte = (uint8_t)(speed & 0xff);
-        uint8_t crc = id + 0x05 + 0x03 + 0x20 + lowByte + highByte;
-        uint8_t tx[] = {0xff, 0xff, id, 0x05, 0x03, 0x20, lowByte,
-                               highByte, ~crc};
+    /*
+     * Make new packet
+     */
+    static const uint8_t DYN_SET_ANGLE_CMD_LEN = 9;
+    uint8_t highByte = (uint8_t)((speed >> 8) & 0xff);
+    uint8_t lowByte = (uint8_t)(speed & 0xff);
+    uint8_t crc = id + 0x05 + 0x03 + 0x20 + lowByte + highByte;
+    uint8_t tx[] = {0xff, 0xff, id, 0x05, 0x03, 0x20, lowByte,
+                           highByte, ~crc};
 
-        dyn_send_cmd(tx, DYN_SET_ANGLE_CMD_LEN);
-        dyn_delay(48000000/1000);
-        return;
+    dyn_send_cmd(tx, DYN_SET_ANGLE_CMD_LEN);
+    dyn_delay(48000000/1000);
+    return;
 }
 
 static void dyn_disable_torque(void)
 {
-        static const uint8_t DYN_DISABLE_TORQUE_CMD_LEN = 8;
-        uint8_t crc = (0xfe + 0x04 + 0x03 + 0x18 + 0x00) % 0xff;
-        uint8_t tx[] = {0xff, 0xff, 0xfe, 0x04, 0x03, 0x18, 0x00,
-                                ~crc};
-        dyn_send_cmd(tx, DYN_DISABLE_TORQUE_CMD_LEN);
-        dyn_delay(48000000/1000);
-        return;
+    static const uint8_t DYN_DISABLE_TORQUE_CMD_LEN = 8;
+    uint8_t crc = (0xfe + 0x04 + 0x03 + 0x18 + 0x00) % 0xff;
+    uint8_t tx[] = {0xff, 0xff, 0xfe, 0x04, 0x03, 0x18, 0x00,
+                            ~crc};
+    dyn_send_cmd(tx, DYN_DISABLE_TORQUE_CMD_LEN);
+    dyn_delay(48000000/1000);
+    return;
 }
 
 /*
@@ -69,75 +69,75 @@ static void dyn_disable_torque(void)
  */
 void dynamixel_init(void *args)
 {
-        (void) args;
-        //int i = 0;
+    (void) args;
+    //int i = 0;
 
-        /*
-         * Initialization code
-         * Setting USART pin
-         */
-        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-        /*
-         * USART1_TX_RX setting
-         */
-        LL_GPIO_SetPinMode(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
-                           LL_GPIO_MODE_ALTERNATE);
-        LL_GPIO_SetAFPin_8_15(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
-                              DYNAMIXEL_USART_PIN_AF);
-        LL_GPIO_SetPinSpeed(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
-                            LL_GPIO_SPEED_FREQ_HIGH);
-        LL_GPIO_SetPinOutputType(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
-                                 LL_GPIO_OUTPUT_OPENDRAIN);
-        /*
-         * USART Setting
-         */
-        LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_USART1);
-        LL_USART_SetParity(DYNAMIXEL_USART, DYNAMIXEL_USART_PARITY);
-        LL_USART_SetDataWidth(DYNAMIXEL_USART, DYNAMIXEL_USART_DATA_WIDTH);
-        LL_USART_SetStopBitsLength(DYNAMIXEL_USART, DYNAMIXEL_USART_STOPBITS);
-        LL_USART_SetBaudRate(DYNAMIXEL_USART, SystemCoreClock,
-                             DYNAMIXEL_USART_OVERSAMPL,
-                             DYNAMIXEL_USART_BAUDRATE);
-        LL_USART_EnableDMAReq_RX(DYNAMIXEL_USART);
-        LL_USART_EnableHalfDuplex(DYNAMIXEL_USART);
-        LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
-        LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
-        /*
-         * Configure debug led
-         */
-        LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
-        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
-        LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
-        LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
-        LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
-        return;
+    /*
+     * Initialization code
+     * Setting USART pin
+     */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    /*
+     * USART1_TX_RX setting
+     */
+    LL_GPIO_SetPinMode(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
+                       LL_GPIO_MODE_ALTERNATE);
+    LL_GPIO_SetAFPin_8_15(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
+                          DYNAMIXEL_USART_PIN_AF);
+    LL_GPIO_SetPinSpeed(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
+                        LL_GPIO_SPEED_FREQ_HIGH);
+    LL_GPIO_SetPinOutputType(DYNAMIXEL_USART_PORT, DYNAMIXEL_USART_PIN,
+                             LL_GPIO_OUTPUT_OPENDRAIN);
+    /*
+     * USART Setting
+     */
+    LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_USART1);
+    LL_USART_SetParity(DYNAMIXEL_USART, DYNAMIXEL_USART_PARITY);
+    LL_USART_SetDataWidth(DYNAMIXEL_USART, DYNAMIXEL_USART_DATA_WIDTH);
+    LL_USART_SetStopBitsLength(DYNAMIXEL_USART, DYNAMIXEL_USART_STOPBITS);
+    LL_USART_SetBaudRate(DYNAMIXEL_USART, SystemCoreClock,
+                         DYNAMIXEL_USART_OVERSAMPL,
+                         DYNAMIXEL_USART_BAUDRATE);
+    LL_USART_EnableDMAReq_RX(DYNAMIXEL_USART);
+    LL_USART_EnableHalfDuplex(DYNAMIXEL_USART);
+    LL_USART_DisableDirectionTx(DYNAMIXEL_USART);
+    LL_USART_EnableDirectionRx(DYNAMIXEL_USART);
+    /*
+     * Configure debug led
+     */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_9, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_9, LL_GPIO_OUTPUT_PUSHPULL);
+    LL_GPIO_SetPinOutputType(GPIOC, LL_GPIO_PIN_8, LL_GPIO_OUTPUT_PUSHPULL);
+    return;
 }
 
 void dyn_set_angle(void *args)
 {
-        /*
-         * Make new packet
-         */
-        static const uint8_t DYN_SET_ANGLE_CMD_LEN = 9;
-        dyn_set_angle_t *cmd_args = (dyn_set_angle_t *) args;
-        uint8_t highByte = (uint8_t)((cmd_args->angle >> 8) & 0xff);
-        uint8_t lowByte = (uint8_t)(cmd_args->angle & 0xff);
-        uint8_t crc = cmd_args->id + 0x05 + 0x03 + 0x1e + lowByte + highByte;
-        uint8_t tx[] = {0xff, 0xff, cmd_args->id, 0x05, 0x03, 0x1e, lowByte,
-                               highByte, ~crc};
+    /*
+     * Make new packet
+     */
+    static const uint8_t DYN_SET_ANGLE_CMD_LEN = 9;
+    dyn_set_angle_t *cmd_args = (dyn_set_angle_t *) args;
+    uint8_t highByte = (uint8_t)((cmd_args->angle >> 8) & 0xff);
+    uint8_t lowByte = (uint8_t)(cmd_args->angle & 0xff);
+    uint8_t crc = cmd_args->id + 0x05 + 0x03 + 0x1e + lowByte + highByte;
+    uint8_t tx[] = {0xff, 0xff, cmd_args->id, 0x05, 0x03, 0x1e, lowByte,
+                           highByte, ~crc};
 
-        if (cmd_args->id == 255 && cmd_args->angle == 0){
-                dyn_disable_torque();
-                LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
-                return;
-        }
+    if (cmd_args->id == 255 && cmd_args->angle == 0){
+            dyn_disable_torque();
+            LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+            return;
+    }
 
-        if (cmd_args->angle > DYN_MAX_ANGLE)
-                return;
-        
-        LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_9);
-        if (cmd_args->speed != 0)
-            dyn_set_speed(cmd_args->id, cmd_args->speed);
-        dyn_send_cmd(tx, DYN_SET_ANGLE_CMD_LEN);
-        return;
+    if (cmd_args->angle > DYN_MAX_ANGLE)
+            return;
+
+    LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_9);
+    if (cmd_args->speed != 0)
+        dyn_set_speed(cmd_args->id, cmd_args->speed);
+    dyn_send_cmd(tx, DYN_SET_ANGLE_CMD_LEN);
+    return;
 }
--- main.c
+++ main.c
@@ -21,57 +21,57 @@
   */
 static void rcc_config(void)
 {
-        /* Set FLASH latency */
-        LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+    /* Set FLASH latency */
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
 
-        /* Enable HSI and wait for activation*/
-        LL_RCC_HSI_Enable();
-        while (LL_RCC_HSI_IsReady() != 1);
+    /* Enable HSI and wait for activation*/
+    LL_RCC_HSI_Enable();
+    while (LL_RCC_HSI_IsReady() != 1);
 
-        /* Main PLL configuration and activation */
-        LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
-                                    LL_RCC_PLL_MUL_12);
+    /* Main PLL configuration and activation */
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
+                                LL_RCC_PLL_MUL_12);
 
-        LL_RCC_PLL_Enable();
-        while (LL_RCC_PLL_IsReady() != 1);
+    LL_RCC_PLL_Enable();
+    while (LL_RCC_PLL_IsReady() != 1);
 
-        /* Sysclk activation on the main PLL */
-        LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-        LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-        while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+    /* Sysclk activation on the main PLL */
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
 
-        /* Set APB1 prescaler */
-        LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+    /* Set APB1 prescaler */
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
 
-        /* Update CMSIS variable (which can be updated also
-         * through SystemCoreClockUpdate function) */
-        SystemCoreClock = 48000000;
+    /* Update CMSIS variable (which can be updated also
+     * through SystemCoreClockUpdate function) */
+    SystemCoreClock = 48000000;
 }
 
 int main(void)
 {
-        uint8_t col_av_status = 0;
-        uint8_t cur_id = 0;
-        uint8_t err_man_status;
+    uint8_t col_av_status = 0;
+    uint8_t cur_id = 0;
+    uint8_t err_man_status;
 
-        rcc_config();
-        term_init();
-        coll_avoid_init();
-        err_man_init();
-        while (1) {
-                col_av_status = col_av_read_status();
-                if (col_av_status) {
-                        col_av_set_block();
-                        while (col_av_status >> 1 && cur_id++);
-                        //reset_sensor(cur_id);
-                        col_av_clr_status(cur_id);
-                        col_av_clr_block();
-                }
-                err_man_status = er_man_disp_get();
-                if (err_man_status) {
-                        err_man_show_err();
-                        er_man_disp_clr();
-                }
+    rcc_config();
+    term_init();
+    coll_avoid_init();
+    err_man_init();
+    while (1) {
+        col_av_status = col_av_read_status();
+        if (col_av_status) {
+            col_av_set_block();
+            while (col_av_status >> 1 && cur_id++);
+            //reset_sensor(cur_id);
+            col_av_clr_status(cur_id);
+            col_av_clr_block();
         }
-        return 0;
+        err_man_status = er_man_disp_get();
+        if (err_man_status) {
+            err_man_show_err();
+            er_man_disp_clr();
+        }
+    }
+    return 0;
 }
