34ee4d6c0f0c381f690955e3c5b51736e5cf7e05
nikigor <n.gorbadey@gmail.com>
2019-04-07 13:17:17 +0300

    proximity: fast reload
    1) Add fast reload of proximity sensors
    2) Display debug information
    
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- lib/dev_map.h
+++ lib/dev_map.h
@@ -2,6 +2,6 @@
 #define _DEV_MAP_H_
 
 #define NUMBER_OF_DYNAMIXELS            4
-#define NUMBER_OF_PROX_SENSORS          3
+#define NUMBER_OF_PROX_SENSORS          6
 
 #endif //_DEV_MAP_H_
--- lib/err_manager.h
+++ lib/err_manager.h
@@ -11,16 +11,22 @@
     (((flag) >> (n)) & 0x01)
 
 typedef struct {
-    uint8_t col_av_err_flags;
+    uint8_t col_av_err_num;
+    uint8_t reset_id;
+    uint8_t broken_num;
+    int reset_time;
     uint8_t col_av_dist[NUMBER_OF_PROX_SENSORS];
+    uint8_t col_av_status[NUMBER_OF_PROX_SENSORS];
     uint8_t disp_update;
 } err_ctrl_t;
 
 void err_man_init(void);
 void err_man_set_dist(uint8_t *dist, uint8_t len);
+void err_man_set_time(int time);
 void err_man_show_err(void);
 uint8_t er_man_disp_get(void);
 void er_man_disp_set(void);
 void er_man_disp_clr(void);
+void er_man_add_err(uint8_t *status, uint8_t broken);
 
 #endif //_ERR_MANAGER_H_
